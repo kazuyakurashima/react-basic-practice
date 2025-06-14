08655cf73b475c2246d723ff3dc668718b6e7168
kazuyakurashima <gs.kaz224@gmail.com>
2025-06-15 08:43:07 +0900

    🌱Basic JSX render test with App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const App = () => {
+  return (
+    <>
+      <h1>ヤッホー</h1>
+      <p>お元気</p>
+    </>
+  )
+}
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+      <App />
+  </StrictMode>
+);
+
+
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
 
 // main.jsx・・・Reactのエントリーポイント。
