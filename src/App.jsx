da66b4c2285026dc3c0f83aca5027c82e0a0d760
kazuyakurashima <gs.kaz224@gmail.com>
2025-06-17 11:13:08 +0900

    ✨ ColorfulMessageコンポーネントを作成＆Appで使用して表示確認完了

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
   const onClickButton = () => alert();
-
-    const contentStyleB = {
-    color:"green",
-    fontSize:"18px",
-  };
   return (
     <>
       <h1 style={{ color:"red" }}>こんにちは</h1>
-      <ColorfulMessage color="blue" message="お元気ですか" />
-      <p style={contentStyleB}>元気です！</p>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="green">元気です</ColorfulMessage>
       <button onClick = {onClickButton}>ボタン</button>
     </>
   );
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,10 +1,10 @@
 export const ColorfulMessage = (props) => {
-    const contentStyleA = {
+    const contentStyle = {
     color: props.color,
     fontSize:"18px",
     };
 
     return (
-      <p style={contentStyleA}>{props.message}</p>
-    )
+        <p style={contentStyle}>{props.children}</p>
+    );
 };
